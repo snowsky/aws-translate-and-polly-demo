0e64da56b63a8dc9a06d03b170388af45c70b358
tetreault <tetreault1@gmail.com>
2018-07-24 19:05:20 -0400

    init commit

---
+++ aws-polly-translate-site/README.md
@@ -0,0 +1,22 @@
+# aws-polly-translate-site
+
+> English-to-Russian Text-to-speech synthesis demo using AWS Translate and AWS Polly.
+
+## Build Setup
+
+``` bash
+# install dependencies
+$ yarn install
+
+# serve with hot reload at localhost:3000
+$ yarn run dev
+
+# build for production and launch server
+$ yarn run build
+$ yarn start
+
+# generate static project
+$ yarn run generate
+```
+
+For detailed explanation on how things work, checkout [Nuxt.js docs](https://nuxtjs.org).
---
+++ aws-polly-translate-site/pages/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <section class="section">
+    <div class="hero is-fullheight">
+      <div class="hero-head">
+        <div class="container">
+          <br>
+          <h1 class="title has-text-centered has-text-white">Text-to-Speech Synthesis - English to Russian</h1>
+        </div>
+      </div>
+      <div class="hero-body">
+        <div class="container">
+          <div class="columns">
+            <div class="column">
+              <label class="subtitle has-text-white">English</label>
+              <textarea 
+                class="textarea is-medium"
+                rows="3" 
+                cols="33" 
+                maxlength="200"
+                wrap="hard">
+              </textarea>
+            </div>
+            <div class="column">
+              <label class="subtitle has-text-white">Russian</label>
+              <textarea 
+                class="textarea is-medium"
+                rows="3" 
+                cols="33" 
+                maxlength="200"
+                wrap="hard">
+              </textarea>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+
+export default {
+  components: {
+  }
+}
+</script>
+
+<style>
+.section {
+  padding: 0;
+  margin: 0;
+  background: blue;
+}
+</style>
